c6f250a0c11dcf58b4490410aac10e50dc95c533
suspiciousRaccoon <127566947+suspiciousRaccoon@users.noreply.github.com>
2025-08-02 14:03:54 -0300

    feat: remove telemetry

--- src/utils/sendTelemetry.ts
+++ src/utils/sendTelemetry.ts
@@ -10,29 +10,5 @@ export interface TelemetryData {
 }
 
 export const sendTelemetry = async (route: string): Promise<void> => {
-  const enabled = process.env.TELEMETRY_ENABLED === undefined || process.env.TELEMETRY_ENABLED === 'true';
-
-  if (!enabled) {
-    return;
-  }
-
-  if (route === '/') {
-    return;
-  }
-
-  const telemetry: TelemetryData = {
-    route,
-    apiVersion: `${packageJson.version}`,
-    timestamp: new Date(),
-  };
-
-  const url =
-    process.env.TELEMETRY_URL && process.env.TELEMETRY_URL !== ''
-      ? process.env.TELEMETRY_URL
-      : 'https://log.evolution-api.com/telemetry';
-
-  axios
-    .post(url, telemetry)
-    .then(() => {})
-    .catch(() => {});
+  return;
 };
